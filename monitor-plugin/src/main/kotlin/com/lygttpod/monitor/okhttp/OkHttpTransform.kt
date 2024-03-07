9f9367faf275d38dfe86c7b9c24f9623cfa4f0b5
mahongyin <1976222027@qq.com>
2024-03-07 17:00:21 +0800

    升级 gradle 、AGP 、kotlin 版本 插件适配AGP8+

--- monitor-plugin/src/main/kotlin/com/lygttpod/monitor/okhttp/OkHttpTransform.kt
+++ monitor-plugin/src/main/kotlin/com/lygttpod/monitor/okhttp/OkHttpTransform.kt
@@ -1,13 +1,23 @@
 package com.lygttpod.monitor.okhttp
 
-import com.quinn.hunter.transform.HunterTransform
-import com.quinn.hunter.transform.RunVariant
-import org.gradle.api.Project
+import com.android.build.api.instrumentation.AsmClassVisitorFactory
+import com.android.build.api.instrumentation.ClassContext
+import com.android.build.api.instrumentation.ClassData
+import com.android.build.api.instrumentation.InstrumentationParameters
+import org.objectweb.asm.ClassVisitor
 
-class OkHttpTransform(project: Project?) : HunterTransform(project) {
-    init { this.bytecodeWeaver = OkHttpWeaver() }
+abstract class OkHttpTransform :
+    AsmClassVisitorFactory<InstrumentationParameters.None> {
+    override fun createClassVisitor(classContext: ClassContext,
+        nextClassVisitor: ClassVisitor): ClassVisitor {
+        //指定真正的ASM转换器
+        return OkHttpClassVisitor(nextClassVisitor)
+    }
 
-    override fun getRunVariant(): RunVariant {
-        return RunVariant.DEBUG
+    // 通过classData中的当前类的信息，用来过滤哪些类需要执行字节码转换，
+    // 这里支持通过类名，包名，注解，接口，父类等属性来组合判断
+    override fun isInstrumentable(classData: ClassData): Boolean {
+        //可以设classData过滤条件
+        return true
     }
 }
--- monitor-plugin/src/main/kotlin/com/lygttpod/monitor/plugin/MonitorPlugin.kt
+++ monitor-plugin/src/main/kotlin/com/lygttpod/monitor/plugin/MonitorPlugin.kt
@@ -1,6 +1,8 @@
 package com.lygttpod.monitor.plugin
 
-import com.android.build.gradle.AppExtension
+import com.android.build.api.instrumentation.FramesComputationMode
+import com.android.build.api.instrumentation.InstrumentationScope
+import com.android.build.api.variant.AndroidComponentsExtension
 import com.lygttpod.monitor.okhttp.OkHttpTransform
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -18,11 +20,17 @@ class MonitorPlugin : Plugin<Project> {
         }
         println("MonitorPlugin---->enableMonitorPlugin = $enableMonitorPlugin")
         if (!enableMonitorPlugin) return
-        try {
-            val appException: AppExtension = project.extensions.getByName("android") as AppExtension
-            appException.registerTransform(OkHttpTransform(project))
-        } catch (e: Exception) {
-            e.printStackTrace()
+
+        //这里appExtension获取方式与原transform api不同，可自行对比
+        val appExtension = project.extensions.getByType(AndroidComponentsExtension::class.java)
+
+        // 注册单个字节码转换器任务
+        appExtension.onVariants { variant ->
+            //可以通过variant来获取当前编译环境的一些信息，最重要的是可以 variant.name 来区分是debug模式还是release模式编译
+            variant.instrumentation.transformClassesWith(OkHttpTransform::class.java, InstrumentationScope.ALL) {
+            }
+            //InstrumentationScope.ALL 配合 FramesComputationMode.COPY_FRAMES可以指定该字节码转换器在全局生效，包括第三方lib
+            variant.instrumentation.setAsmFramesComputationMode(FramesComputationMode.COPY_FRAMES)
         }
     }
 
